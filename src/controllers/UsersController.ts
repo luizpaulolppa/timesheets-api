715b55b3cc4a5a53e0306d3dfb400867c466a06d
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-07-19 13:56:09 -0300

    criando novos DTOs

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,11 +1,15 @@
 import { Router, Request, Response } from 'express';
 import HttpStatus from 'http-status-codes';
+import UserService from '../services/UserService';
 
 const usersRouter = Router();
 
+const userService = new UserService();
+
 usersRouter.post('/', async (request: Request, response: Response) => {
   const { name, email, password } = request.body;
-  return response.status(HttpStatus.CREATED).json({ user: {} });
+  const user = await userService.createNewUser({ name, email, password });
+  return response.status(HttpStatus.CREATED).json({ user });
 });
 
 export default usersRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
-import express from 'express';
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+import express, { NextFunction, Response, Request } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import "express-async-errors";
 import routes from './routes';
 import "reflect-metadata";
 import './database'
+import AppError from './errors/AppError';
 
 const app = express();
 const PORT = process.env.PORT || 3333;
@@ -13,6 +18,20 @@ app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, _request: Request, response: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message
+    });
+  }
+
+  return response.status(500).json({
+    status: 500,
+    message: "Internal server error"
+  })
+});
 
 app.listen(PORT, () => {
   console.log('Server started on port 3333!');
